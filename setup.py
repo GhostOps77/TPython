69f3202d8bf2a59a42541fcedd320e2c5abda3f0
Techlord210 <techlord210@gmail.com>
2023-02-20 22:30:49 +0530

    Update 1.7
    config parser is now implemented in config.py
    no longer uses jsonc-parser.
    have a exception prettifier now.
    renamed vars.
    uses timeit lib for timeit now.
    version command now also python version.
    exits on EOFError now.

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import sys
 from setuptools import setup
 
 with open('README.md', 'r') as f:
@@ -6,39 +7,43 @@ with open('README.md', 'r') as f:
 with open('requirements.txt', 'r') as f:
     DEPENDENCIES = f.read().splitlines()
 
-__version__ = '1.7'
+python_version = sys.version_info
+__version__ = '1.8'
 
 if __name__ == '__main__':
-    setup(
-        name = 'TPython',
-        version = __version__,
-        description = 'A better python REPL',
-        long_description = LD,
-        long_description_content_type = 'text/markdown',
-        url = 'https://github.com/Techlord210/TPython',
-        author = 'Techlord210',
-        author_email = 'techlord210@gmail.com',
-        license = 'MIT',
-        classifiers = [
-                "Development Status :: 5 - Production/Stable",
-                'Environment :: Console',
-                "Intended Audience :: Developers",
-                "Intended Audience :: Education",
-                "Intended Audience :: Other Audience",
-                "Programming Language :: Python :: 3",
-                "Programming Language :: Python :: 3.6",
-                "Programming Language :: Python :: 3.7",
-                "Programming Language :: Python :: 3.8",
-                "Programming Language :: Python :: 3.9",
-                "Programming Language :: Python :: 3.10",
-                "Programming Language :: Python :: 3.11",
-                "License :: OSI Approved :: MIT License"
-            ],
-        install_requires = DEPENDENCIES,
-        entry_points = {
-            'console_scripts': [
-                'tpy = tpy.tpy:main'
-                ]
-            },
-        python_requires = ">=3.6"
-    )
+    if python_version.major == 3 and python_version.minor >= 6:
+        setup(
+            name = 'TPython',
+            version = __version__,
+            description = 'A better python REPL',
+            long_description = LD,
+            long_description_content_type = 'text/markdown',
+            url = 'https://github.com/Techlord210/TPython',
+            author = 'Techlord210',
+            author_email = 'techlord210@gmail.com',
+            license = 'MIT',
+            classifiers = [
+                    'Development Status :: 5 - Production/Stable',
+                    'Environment :: Console',
+                    'Intended Audience :: Developers',
+                    'Intended Audience :: Education',
+                    'Intended Audience :: Other Audience',
+                    'Operating System :: OS Independent',
+                    'Programming Language :: Python :: 3',
+                    'Programming Language :: Python :: 3.6',
+                    'Programming Language :: Python :: 3.7',
+                    'Programming Language :: Python :: 3.8',
+                    'Programming Language :: Python :: 3.9',
+                    'Programming Language :: Python :: 3.10',
+                    'Programming Language :: Python :: 3.11',
+                    'License :: OSI Approved :: MIT License'
+                ],
+            install_requires = DEPENDENCIES,
+            entry_points = {
+                'console_scripts': [
+                    'tpy = tpy.tpy:main'
+                    ]
+                }
+        )
+    else:
+        sys.exit('requires python version 3.6 or above.\ncurrent version: {major}.{minor}'.format(major=python_version.major, minor=python_version.minor))
